8695d0d641aa1f4d1d9b02bd25f9c7d6e37d1538
N L <lazarenkonikita1993@gmail.com>
2024-12-15 01:31:31 +0000

    Revert "Add web server with non-functional GUI"
    This reverts commit 9b1f2aa696f30f892c31ac62dcc3b264de6d6cbc.

--- server.js
+++ server.js
@@ -16,41 +16,30 @@ const settings = commonSettings;
 
 import net from 'net';
 import { processQueue } from './server-core.js';
+
 import { connections } from './server-core.js';
+
 import Gpu from './gpu.js';
 import Cpu from './cpu.js';
 import ComputationalUnit from './computational-unit.js';
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 import restRoutes from './restApi.js';
-import open from 'open';
 
 const app = express();
 const port = 2604;
 
-// Serve static files from the React app build directory
-const buildPath = path.join(path.resolve(), 'gui/build');
-app.use(express.static(buildPath));
-
 app.use(bodyParser.json());
+app.use('/', restRoutes);
 
-app.use('/api', restRoutes);
-
-// Serve the index.html file for any other routes
-app.get('*', (req, res) => {
-	console.log(`WEB Server listening on port ${port}`);
-	res.sendFile(path.join(buildPath, 'index.html'));
-});
-
-app.listen(port, '0.0.0.0', () => {
-	console.log(`REST API Server listening on port ${port} with /api path`);
-	open(`http://localhost:${port}`);
-});
 
 const delimiter = '\n\n\n\n';
 let workstations = [];
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`REST API Server listening on port ${port}`);
+});
 
 /**
  * @typedef {object} Message
@@ -70,122 +59,135 @@ let workstations = [];
 const log = console.log;
 
 class Connection {
-	/**
-	 * @typedef {'empty' | 'ready'} State
-	 */
 
-	/**
-	 * @param {net.Socket} socket
-	 */
-	constructor(socket, onReady) {
-		this.socket = socket;
+    /**
+     * @typedef {'empty' | 'ready'} State
+     */
 
-		/** @type {State} */
-		this.state = 'empty';
+    /**
+     * @param {net.Socket} socket
+     */
+    constructor(socket, onReady) {
+        this.socket = socket;
 
-		this.onReady = onReady;
-		this.doneCallbacks = {};
-		this.messageBuffer = '';
+        /** @type {State} */
+        this.state = 'empty';
 
-		socket.on('data', (data) => this.onData(data));
+        this.onReady = onReady;
+        this.doneCallbacks = {};
+        this.messageBuffer = '';
 
-		socket.write(JSON.stringify({
-			command: 'Hello',
-		}) + delimiter);
-	}
+        socket.on('data', (data) => this.onData(data));
 
-	get address() {
-		return this.socket.remoteAddress + ':' + this.socket.remotePort;
-	}
+        socket.write(JSON.stringify({
+            command: 'Hello',
+        }) + delimiter);
+    }
 
-	disconnect() {
-		this.socket.destroy();
-	}
+    get address() {
+        return this.socket.remoteAddress + ':' + this.socket.remotePort;
+    }
 
-	/**
-	 * @param {Buffer} dataString
-	 */
-	processMessage(dataString) {
-		/** @type {Message} */
-		const data = JSON.parse(dataString.toString());
+    disconnect() {
+        this.socket.destroy();
+    }
 
-		switch (this.state) {
-			case 'empty': {
-				if (data.command === 'Hello') {
-					this.name = data.name || this.address;
-					this.state = 'ready';
-					this.gpus = data.gpus;
-					this.cpus = data.cpus;
-					this.onReady(this, data.override);
-				} else {
-					log('Unknown command: ', data.command);
-				}
-			} break;
-			case 'ready':
-				if (data.command === 'TaskFinished') {
-					console.log({ cb: this.doneCallbacks, data });
-					const onDone = this.doneCallbacks[data.taskId];
-					delete this.doneCallbacks[data.taskId];
-					if (onDone) {
-						onDone(data.result, data.isError);
-					}
-				} else {
-					log('Unknown command: ', data.command);
-				}
-				break;
-			default:
-				break;
-		}
-	}
+    /**
+     * @param {Buffer} dataString
+     */
+    processMessage(dataString) {
+        /** @type {Message} */
+        const data = JSON.parse(dataString.toString());
 
-	/** @param {Buffer} dataBuffer */
-	onData(dataBuffer) {
-		const dataString = this.messageBuffer + dataBuffer.toString();
-		let index = 0;
+        switch (this.state) {
+            case 'empty': {
 
-		while (dataString.includes(delimiter, index)) {
-			const endIndex = dataString.indexOf(delimiter, index);
-			const message = dataString.substring(index, endIndex);
-			this.processMessage(message);
-			index = endIndex + delimiter.length;
-		}
+                if (data.command === 'Hello') {
 
-		if (index < dataString.length) {
-			this.messageBuffer = dataString.substring(index);
-		} else {
-			this.messageBuffer = '';
-		}
-	}
+                    this.name = data.name || this.address;
+                    this.state = 'ready';
+                    this.gpus = data.gpus;
+                    this.cpus = data.cpus;
+                    this.onReady(this, data.override);
+                } else {
 
-	/**
-	 * @param {Task} task
-	 * @param {Array<ComputationalUnit>} resources
-	 * @param {Function} onDone
-	 */
-	sendTask(task, resources, onDone) {
-		if (this.state === 'ready') {
-			/** @type {Message} */
-			const message = {
-				command: 'Process',
-				task: task.describe(),
-				resources: resources.map(
-					a => ({
-						type: a.type,
-						id: a.id,
-					})
-				)
-			};
+                    log('Unknown command: ', data.command);
+                }
+            } break;
+            case 'ready':
 
-			this.socket.write(JSON.stringify(message) + delimiter);
-			this.doneCallbacks[task.taskId] = onDone;
-		} else {
-			throw new Error(`'${this.name}' is not ready`);
-		}
-	}
+                if (data.command === 'TaskFinished') {
 
-	log() {
-		log(this.address + ` (${this.id}) ` + this.state);
-	}
+                    console.log({cb: this.doneCallbacks, data});
+
+                    const onDone = this.doneCallbacks[data.taskId];
+                    delete this.doneCallbacks[data.taskId];
+
+                    if (onDone) {
+                        onDone(data.result, data.isError);
+                    }
+                    
+                } else {
+                    log('Unknown command: ', data.command);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /** @param {Buffer} dataBuffer */
+    onData(dataBuffer) {
+        const dataString = this.messageBuffer + dataBuffer.toString();
+        let index = 0;
+    
+        while (dataString.includes(delimiter, index)) {
+    
+            const endIndex = dataString.indexOf(delimiter, index);
+            const message = dataString.substring(index, endIndex);
+    
+            this.processMessage(message);
+    
+            index = endIndex + delimiter.length;
+        }
+    
+        if (index < dataString.length) {
+            this.messageBuffer = dataString.substring(index);
+        } else {
+            this.messageBuffer = '';
+        }
+    }
+
+    /**
+     * @param {Task} task
+     * @param {Array<ComputationalUnit>} resources
+     * @param {Function} onDone
+     */
+    sendTask(task, resources, onDone) {
+        if (this.state === 'ready') {
+            /** @type {Message} */
+            const message = {
+                command: 'Process',
+                task: task.describe(),
+                resources: resources.map(
+                    a => ({
+                        type: a.type,
+                        id: a.id,
+                    })
+                )
+            };
+
+            this.socket.write(JSON.stringify(message) + delimiter);
+
+            this.doneCallbacks[task.taskId] = onDone;
+        } else {
+            throw new Error(`'${this.name}' is not ready`);
+        }
+    }
+
+    log() {
+        log(this.address + ` (${this.id}) ` + this.state);
+    }
 }
 
 /**
@@ -193,75 +195,86 @@ class Connection {
  */
 
 const server = net.createServer(socket => {
-	/**
-	 * @param {Connection} connection
-	 * @param {boolean} override
-	 */
-	const onReady = (connection, override = false) => {
-		if (connection.name in connections) {
-			if (override) {
-				log('Overriding same connection from ' + connection.name);
-				const oldConnection = connections[connection.name];
-				workstations = workstations.filter(
-					a => a.name !== oldConnection.name
-				);
-				oldConnection.name = undefined;
-				oldConnection.disconnect();
-				connections[connection.name] = connection;
-			} else {
-				log('Closing new same connection from ' + connection.name);
-				connection.disconnect();
-				return;
-			}
-		} else {
-			connections[connection.name] = connection;
-			log(connection.name + ' ready');
-		}
+    /**
+     * @param {Connection} connection
+     * @param {boolean} override
+     */
+    const onReady = (connection, override = false) => {
+        if (connection.name in connections) {
+            if (override) {
+                log('Overriding same connection from ' + connection.name);
 
-		workstations.push({
-			name: connection.name,
-			gpus: {
-				shared: connection.gpus.map(
-					a => new Gpu(a.id, connection.name, a.memory, [], a.maxProcesses)
-				),
-				personal: []
-			},
-			cpus: connection.cpus.map(
-				a => new Cpu(a.id, connection.name, [], a.maxProcesses)
-			)
-		});
+                const oldConnection = connections[connection.name];
 
-		console.log(workstations);
-		console.log(workstations.length);
+                workstations = workstations.filter(
+                    a => a.name !== oldConnection.name
+                );
 
-		processQueue();
-	};
+                oldConnection.name = undefined;
+                oldConnection.disconnect();
 
-	const connection = new Connection(socket, onReady);
-	const address = connection.address;
-	log('New connection: ' + address);
+                connections[connection.name] = connection;
+            } else {
+                log('Closing new same connection from ' + connection.name);
+                connection.disconnect();
+                return;
+            }
+        } else {
+            connections[connection.name] = connection;
+            log(connection.name + ' ready');
+        }
 
-	const close = () => {
-		workstations = workstations.filter(
-			a => a.name !== connection.name
-		);
-		console.log(workstations);
-		console.log(workstations.length);
-		log('Closed ' + address);
-	};
+        workstations.push({
+            name: connection.name,
+            gpus: {
+                shared: connection.gpus.map(
+                    a => new Gpu(a.id, connection.name, a.memory, [], a.maxProcesses)
+                ),
+                personal: []
+            },
+            cpus: connection.cpus.map (
+                a => new Cpu(a.id, connection.name, [], a.maxProcesses)
+            )
+        });
 
-	socket.on('close', close);
+        console.log(workstations);
+        console.log(workstations.length);
 
-	socket.on('error', /** @param {{code: string}} error */ error => {
-		switch (error.code) {
-			case 'ECONNRESET':
-				break;
-			default:
-				log(error.code);
-		}
-	});
+        processQueue();
+    };
+
+    const connection = new Connection(socket, onReady);
+
+    const address = connection.address;
+
+    log('New connection: ' + address);
+
+    const close = () => {
+
+        workstations = workstations.filter(
+            a => a.name !== connection.name
+        );
+
+        console.log(workstations);
+        console.log(workstations.length);
+
+        log('Closed ' + address);
+    };
+
+    socket.on('close', close);
+
+    socket.on('error', /** @param {{code: string}} error */ error => {
+        
+        switch (error.code) {
+            case 'ECONNRESET':
+                break;
+            default:
+                log(error.code);
+        }
+    });
 });
 
 server.on('error', console.log);
+
 server.listen(settings.ports.server, '0.0.0.0');
 console.log('Task server started at port: ' + settings.ports.server);
